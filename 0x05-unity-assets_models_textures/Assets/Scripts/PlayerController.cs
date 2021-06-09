d50d351af279fad955bf894990571bd0ee5f200f
Valentina Rua C <40582270+valerc123@users.noreply.github.com>
2021-06-08 21:06:34 -0500

    position camera

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update\
+    public Rigidbody rb;
+    private float movementSpeed = 5f;
+    private Vector3 move;
+
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>();
+    }
+
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+    
+        transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, 0,  verticalInput * movementSpeed * Time.deltaTime);
+        
+        if (Input.GetKey(KeyCode.Space)) {
+           // move = new Vector3()
+           rb.AddForce(Vector3.up * movementSpeed);
+
+        } 
+    }
+}
