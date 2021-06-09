2ea23b4266dc2816890aeab694c8bee9998d6c2d
Valentina Rua C <40582270+valerc123@users.noreply.github.com>
2021-06-08 21:29:44 -0500

    camera controller

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+        float horizontalInput = Input.GetAxis("Mouse X");
+        transform.Rotate(0, horizontalInput, 0);
+    }
+}
