c48d2df36b6fee8e82d185e110ef698fa1949b9b
Valentina Rua C <40582270+valerc123@users.noreply.github.com>
2021-06-08 22:20:05 -0500

    timer canvas

--- 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,15 +9,14 @@ public class PlayerController : MonoBehaviour
     public Rigidbody rb;
     private float movementSpeed = 5f;
     private Vector3 move;
+    public int jump;
+    public float jumpHeight = 6f;
+    public bool isGrounded = false;
 
     void Start()
     {
        rb = GetComponent<Rigidbody>();
     }
-
-    
-
-
     // Update is called once per frame
     void Update()
     {
@@ -24,11 +24,21 @@ public class PlayerController : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
     
         transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, 0,  verticalInput * movementSpeed * Time.deltaTime);
-        
-        if (Input.GetKey(KeyCode.Space)) {
-           // move = new Vector3()
-           rb.AddForce(Vector3.up * movementSpeed);
+        transform.rotation = Quaternion.Euler(0,0,0);
+        if ((Input.GetKey(KeyCode.Space)) && (isGrounded == true)) {
+            gameObject.transform.Translate(0, 0.1f , 0);
+          //  rb.AddForce(Vector3.up * jumpHeight);
+        }
 
-        } 
+        if(transform.position.y <= -10){
+            transform.position = new Vector3(0,20,0);
+        }
+    }
+    public void OnCollisionEnter(Collision hit){
+        if (hit.gameObject.CompareTag("ground")){
+            isGrounded = true;
+        }else{
+            isGrounded = false;
+        }
     }
 }
