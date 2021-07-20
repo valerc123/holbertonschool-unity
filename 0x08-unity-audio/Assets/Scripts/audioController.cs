78bb997da7744a7b7c18323dda61ac417fbc6aff
Valentina Rua C <40582270+valerc123@users.noreply.github.com>
2021-07-19 21:11:32 -0500

    add audio mixer

--- 0x08-unity-audio/Assets/Scripts/audioController.cs
+++ 0x08-unity-audio/Assets/Scripts/audioController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class audioController : MonoBehaviour
 {
-    public GameObject menuSfx;
     private AudioSource menuAudio;
+    public AudioClip [] menuClips;
     void Start()
     {
         menuAudio = GetComponent<AudioSource>();
     }
 
     public void hoverbutton(){
+        menuAudio.clip = menuClips[0];
+        menuAudio.Play();
+    }
+    public void onClickButton(){
+        menuAudio.clip = menuClips[1];
         menuAudio.Play();
     }
 }
