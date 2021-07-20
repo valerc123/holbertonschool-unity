a866b1507cabe631c530ce23e8919e5b17d86dbb
Valentina Rua C <40582270+valerc123@users.noreply.github.com>
2021-07-20 13:00:06 -0500

    snapshots

--- 0x08-unity-audio/Assets/Scripts/PauseMenu.cs
+++ 0x08-unity-audio/Assets/Scripts/PauseMenu.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+# if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 
 
 public class PauseMenu : MonoBehaviour
 {
+  public AudioMixerSnapshot paused;
+  public AudioMixerSnapshot unpaused;
+
   public GameObject canvas;
   public GameObject player;
   public Timer script;
@@ -18,11 +26,27 @@ public class PauseMenu : MonoBehaviour
       Pause();
     }
   }
+
+  void TransitionTo (){
+
+  }
+
+
+
   public void Pause(){
     script.enabled = false;
     canvas.SetActive(true);
+    Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+    Lowpass();
   }
 
+  void Lowpass(){
+    if (Time.timeScale == 0){
+      paused.TransitionTo(.01f);
+    }else{
+      unpaused.TransitionTo(.01f);
+    }
+  }
   public void Resume(){
     script.enabled = true;
     canvas.SetActive(false);
