2133a1883b94d811a1597757eaf0edb935bf168b
Valentina Rua C <40582270+valerc123@users.noreply.github.com>
2021-07-22 16:24:13 -0500

    sounds

--- 0x08-unity-audio/Assets/Scripts/OptionsMenu.cs
+++ 0x08-unity-audio/Assets/Scripts/OptionsMenu.cs
@@ -9,13 +9,31 @@ public class OptionsMenu : MonoBehaviour
   public GameObject InvertToogle;
   public Toggle Toogle;
   private int scene;
+  public Slider soundSlider;
+  public Slider soundSliderSFX;
+  public float soundSliderValueSFX;
+  public float soundSliderValue;
+  private float volumbg;
+  private float volumbgSFX;
   void Start()
   {
+    soundSliderValue = 0;
     scene = PlayerPrefs.GetInt("previousLevel");
     Toogle =  InvertToogle.GetComponent<Toggle>();
     Toogle.isOn = PlayerPrefs.GetInt("inverse") == 1 ? true : false;
+    volumbg = PlayerPrefs.GetFloat("bgsoundvolum");
+    soundSlider.value = volumbg;
+
+    volumbgSFX = PlayerPrefs.GetFloat("SFXsoundVolum");
+    soundSliderSFX.value = volumbgSFX;
   }
   void Update(){
+    soundSliderValue = soundSlider.value;
+    PlayerPrefs.SetFloat("backgroundSound", soundSliderValue);
+
+    soundSliderValueSFX = soundSliderSFX.value;
+    PlayerPrefs.SetFloat("SFXsound", soundSliderValueSFX);
+
     if(Toogle.isOn == true){
       PlayerPrefs.SetInt("inverse", 1);
       Debug.Log("inverse");
---
+++ 0x08-unity-audio/Assets/bgSound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class bgSound : MonoBehaviour
+{
+    public float backgroundSound;
+    private float bgdb;
+    public AudioMixer mixerbg;
+     public float SFXSound;
+    private float SFXdb;
+    public AudioMixer SFXmixer;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        backgroundSound = PlayerPrefs.GetFloat("backgroundSound");
+        bgdb = 20.0f * Mathf.Log10(backgroundSound);
+        mixerbg.SetFloat("bgsoundmixer", bgdb);
+        PlayerPrefs.SetFloat("bgsoundvolum", backgroundSound);
+
+        SFXSound = PlayerPrefs.GetFloat("SFXsound");
+        SFXdb = 20.0f * Mathf.Log10(SFXSound);
+        SFXmixer.SetFloat("SFXsoundMixer", SFXdb);
+        PlayerPrefs.SetFloat("SFXsoundVolum", SFXSound);
+    }
+}
